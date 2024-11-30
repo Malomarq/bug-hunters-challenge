16ba29c1bdd95db5b95ce8a9e86c32cceecb649f
Malomarq <malomarq@gmail.com>
2024-11-30 21:54:00 +0100

    add apì-25 case

---
+++ cypress/e2e/api-25.spec.js
@@ -0,0 +1,23 @@
+import { taskAPI25 } from "../fixtures";
+
+describe("Task id: api-25", () => {
+    const tests = taskAPI25;
+
+    tests.forEach((test) => {
+        it(`[${test.taskId}] - ${test.envAPI} env`, () => {
+            cy.restoreSetup({ env: test.envAPI });
+            cy.getUsers({ env: test.envAPI, taskId: test.taskId }).then((allUsers) => {
+                const firstUserUuid = allUsers.body.users[0].uuid;
+                cy.getWishlist({ env: test.envAPI, taskId: test.taskId, userUuid: firstUserUuid }).then((getWishlist) => {
+                    const firstGameUuid = getWishlist.body.items[0].uuid;
+                    test.env = Cypress.env(`${test.envAPI}EnvUrl`);
+                    test.url = `/users/${firstUserUuid}${test.url}`;
+                    test.requestBody = { item_uuid: firstGameUuid }
+                    cy.instanceRunner(test);
+                });
+            });
+        });
+    });
+
+    // DEV env: añade items duplicados
+});
--- cypress/utils/zod/schemas/POSTwishlist.js
+++ cypress/utils/zod/schemas/POSTwishlist.js
@@ -15,7 +15,7 @@ export class POSTwishlistSchema extends APIschema {
                         title: z.string().min(1).max(100),
                         uuid: z.string().regex(/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/).min(36).max(36)
                     })
-                ),
+                ).length(3),
                 user_uuid: z.string().regex(/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/).min(36).max(36),
             })
         },
