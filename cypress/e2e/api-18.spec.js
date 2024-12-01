44d30326f4ecf02bb31ad3a6c6d05c9e61b12a7e
Malomarq <malomarq@gmail.com>
2024-12-01 20:30:42 +0100

    add api-18 case

---
+++ cypress/e2e/api-18.spec.js
@@ -0,0 +1,37 @@
+import { taskAPI18 } from "../fixtures";
+
+describe("Task id: api-18", () => {
+    const tests = taskAPI18;
+
+    tests.forEach((test) => {
+        it(`[${test.taskId}] - ${test.envAPI} env`, () => {
+            cy.restoreSetup({ env: test.envAPI });
+            cy.getUsers({ env: test.envAPI, taskId: test.taskId }).then((allUsers) => {
+                const firstUserUuid = allUsers.body.users[0].uuid;
+                cy.getGames({ env: test.envAPI, taskId: test.taskId }).then((allGames) => {
+                    const firstGameUuid = allGames.body.games[0].uuid;
+                    const thirdGameUuid = allGames.body.games[2].uuid;
+                    const firstOrderItems = {
+                        items: [{
+                            item_uuid: firstGameUuid,
+                            quantity: 2,
+                        }, {
+                            item_uuid: thirdGameUuid,
+                            quantity: 1,
+                        }]
+                    };
+
+                    cy.createOrder({ env: test.envAPI, taskId: test.taskId, userUuid: firstUserUuid, orderItems: firstOrderItems }).then((createOrderResponse) => {
+                        const orderUuid = createOrderResponse.body.uuid;
+                        test.env = Cypress.env(`${test.envAPI}EnvUrl`);
+                        test.url = `/orders/${orderUuid}${test.url}`;
+                        test.requestBody = { status: "canceled" };
+                        cy.instanceRunner(test);
+                    });
+                });
+            });
+        });
+    });
+
+    // DEV env: status 422: "Operation forbidden. Order \"status\": the following statuses cannot be changed: completed, canceled". Pero el status es open antes de hacer el cambio
+});
--- cypress/utils/zod/schemas/Orders.js
+++ cypress/utils/zod/schemas/Orders.js
@@ -33,6 +33,23 @@ export class OrdersSchema extends APIschema {
                 code: z.number().refine((val) => val === 400),
                 message: z.string().refine((val) => val.includes("Items with the following \"uuid\" are duplicated"))
             })
+        },
+        this.updateStatusSchema = () => {
+            return z.object({
+                created_at: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/),
+                items: z.array(
+                    z.object({
+                        item_uuid: z.string().regex(/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/).min(36).max(36),
+                        quantity: z.number().min(1).max(100),
+                        total_price: z.number().min(0),
+                    })
+                ).min(1).max(10),
+                status: z.enum(["open", "pending", "overdue", "canceled", "completed"]),
+                total_price: z.number().min(0),
+                updated_at: z.string().regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z$/),
+                user_uuid: z.string().regex(/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/).min(36).max(36),
+                uuid: z.string().regex(/[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}/).min(36).max(36)
+            })
         }
     }
 }
